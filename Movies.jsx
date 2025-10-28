c9472af7c298172609aff587b12a8a03248ca78c
mdmustakim <md4265346@gmail.com>
2025-10-28 14:46:35 +0530

    Add files via upload

---
+++ Movies.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { dummyShowsData } from '../assets/assets'
+import MovieCard from '../Components/MovieCard'
+
+const Movies = () => {
+  return (
+    <div className='px-6 md:px-16 lg:px-24 py-10 text-white'>
+      <h1 className='text-2xl font-bold mb-6'>Now Showing</h1>
+
+      {dummyShowsData.length > 0 ? (
+        <div className='flex flex-wrap gap-8 justify-center'>
+          {dummyShowsData.map((movie) => (
+            <MovieCard key={movie._id} movie={movie} />
+          ))}
+        </div>
+      ) : (
+        <p className='text-gray-400 text-center mt-10'>
+          No movies available right now.
+        </p>
+      )}
+    </div>
+  )
+}
+
+export default Movies
